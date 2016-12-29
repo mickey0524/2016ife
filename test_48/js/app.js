da57b6b9a9f6beadc32a72b8ba53a58774a1e6df
baihao <buptbh@163.com>
2016-12-29 21:20:30 +0800

    无尽版坦克大战，但是技术有限，没能实现平滑过渡

---
+++ test_48/js/app.js
@@ -0,0 +1,65 @@
+
+var imageFactory = new ImageFactory();
+var canvas = document.getElementById('background');
+var peopleCanvas = document.getElementById('people');
+var game = new Game(canvas, peopleCanvas, imageFactory, 6);
+
+
+/**
+ *当图片资源加载完毕后，开始运行游戏
+ */
+var timer = setInterval(function() {
+    if(imageFactory.returnLoadNum()) {
+        clearInterval(timer);
+        window.init();
+    }
+}, 100);
+
+function init() {
+    game.init();
+}
+
+/**
+ *相应上下左右按键，自动寻路过程中，点击Esc退出自动寻路
+ */
+document.onkeydown = function(e) {
+    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+    if(keyCodes[keyCode]) {
+        e.preventDefault();
+        keyStatus[keyCodes[keyCode]] = true;
+    }
+    else if(keyCode == 27) {
+        game.people.route = false;
+        game.people.routeArray = [];
+        keyStatus['left'] = false;
+        keyStatus['right'] = false;
+        keyStatus['top'] = false;
+        keyStatus['bottom'] = false;       
+    }
+    else if(keyCode == 32) {
+        game.bulletPool.use();
+        game.bulletPool.get('green', game.people.x + 15, game.people.y + 15, game.people.rotateAngle);
+    }
+}
+
+document.onkeyup = function(e) {
+    var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+    if(keyCodes[keyCode]) {
+        e.preventDefault();
+        keyStatus[keyCodes[keyCode]] = false;
+    }
+}
+
+/**
+ *点击屏幕，触发自动寻路，按Esc可以退出自动寻路
+ */
+document.onclick = function(e) {
+    var endX = parseInt(e.pageX / map.ceilWidth);
+    var endY = parseInt((e.pageY - 30) / map.ceilHeight);
+    game.people.routeArray = findRoad(parseInt(game.people.x / map.ceilWidth), parseInt(game.people.y / map.ceilHeight), endX, endY);
+    game.people.route = true;
+}
+
+
+
+
---
+++ test_48/js/road.js
@@ -0,0 +1,105 @@
+
+/**
+ *生成自动寻路的路径
+ *@Param {int} (startX, startY) 自动寻路的起点坐标
+ *@Param {int} (endX, endY) 自动寻路的终点坐标
+ *@Return {Array} 自动寻路的路径
+ */ 
+
+function findRoad(startX, startY, endX, endY) {
+    var openList = [];   //尝试访问的节点
+    var closeList = [];  //关闭的节点
+    var result = [];     //结果数组
+    var result_index;    //终点在openList中的索引
+    // console.log('1 ' + startX);
+    // console.log('2 ' + startY);
+    // console.log('3 ' + endX);
+    // console.log('4 ' + endY);
+
+    openList.push({
+        x : startX,
+        y : startY,
+        G : 0
+    });
+
+    do {
+        var curPoint = openList.pop();
+        closeList.push(curPoint);      
+        var pointList = surroundPoint(curPoint);
+        for(var i in pointList) {
+            var item = pointList[i];
+            if(item.x >= 0 && item.x < map.rows && item.y >= 0 && item.y < map.columns &&
+               map.arr[item.x][item.y] != 1 && !existList(item, closeList) && 
+               map.arr[curPoint.x][item.y] != 1 && map.arr[item.x][curPoint.y] != 1) {
+                    var g = curPoint.G + ((curPoint.x - item.x) * (curPoint.y - item.y) == 0 ? 10 : 14);
+                    if(!existList(item, openList)) {
+                        item.G = g;
+                        item.H = Math.abs(endX - item.x) * 10 + Math.abs(endY - item.y) * 10;
+                        item.F = item.G + item.H;
+                        item.parent = curPoint;
+                        openList.push(item);
+                    }
+                    else {
+                        var index = existList(item, openList);
+                        if(g < openList[index].G) {
+                            openList[index].parent = curPoint;
+                            openList[index].G = g;
+                            openList[index].F = g + openList[index].H;
+                        }
+                    }
+            }
+        }
+        if(openList.length == 0) {
+            break;
+        }
+        openList.sort(function(item1, item2) {
+            return item2.F - item1.F;
+        });
+
+    } while(!(result_index = existList({x : endX, y : endY}, openList)));
+
+    if(result_index) {
+        var point = openList[result_index];
+        do {
+            result.unshift({
+                x : point.x,
+                y : point.y
+            });
+            point = point.parent;
+        } while(point.x != startX || point.y != startY);
+    }
+    return result;
+
+}
+
+/**
+ *返回当前被打开的点的周围八个点
+ */
+function surroundPoint(point) {
+    var x = point.x;
+    var y = point.y;
+    return [
+        {x : x - 1, y : y - 1},
+        {x : x, y : y - 1},
+        {x : x + 1, y : y - 1},
+        {x : x + 1, y : y},
+        {x : x + 1, y : y + 1},
+        {x : x, y : y + 1},
+        {x : x - 1, y : y + 1},
+        {x : x - 1, y : y}
+    ];
+}
+
+
+/**
+ *判断point是否存在于list中
+ *
+ */
+function existList(point, list) {
+    for(var i = 0; i < list.length; i++) {
+        if(list[i].x == point.x && list[i].y == point.y) {
+            return i;
+        }
+    }
+    return false;
+}
